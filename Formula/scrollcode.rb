675af3ce6b24a2afdf0c5f31424eac816ba5f14c
moonagic <i@moonagic.com>
2026-03-30 15:10:25 +0800

    Update formula.

--- Formula/scrollcode.rb
+++ Formula/scrollcode.rb
@@ -5,13 +5,13 @@ class Scrollcode < Formula
 
     on_macos do
       on_arm do
-        url "https://github.com/ScrollCode-App/homebrew-scrollcode/releases/download/v0.1.0/ScrollCode_0.1.1_darwin_arm64.tar.gz"
-        sha256 "ARM64_SHA256"
+        url "https://github.com/ScrollCode-App/homebrew-scrollcode/releases/download/v0.1.1/ScrollCode_0.1.1_darwin_arm64.tar.gz"
+        sha256 "50ab30c22e6480ab7bfd20bfec203f024ecf8624809f5c0845db9a0da5420380"
       end
 
       on_intel do
-        url "https://github.com/ScrollCode-App/homebrew-scrollcode/releases/download/v0.1.0/ScrollCode_0.1.1_darwin_amd64.tar.gz"
-        sha256 "AMD64_SHA256"
+        url "https://github.com/ScrollCode-App/homebrew-scrollcode/releases/download/v0.1.1/ScrollCode_0.1.1_darwin_amd64.tar.gz"
+        sha256 "742fec6f88a2be4b7b2152c90caa597d01b764d3dab65ecbcc509c7ab9181683"
       end
     end
 
