da414542254da42fc8f65bd58150b059433d0b45
moonagic <i@moonagic.com>
2026-03-30 18:43:09 +0800

    Update formula.

--- Formula/scrollcode.rb
+++ Formula/scrollcode.rb
@@ -1,4 +1,4 @@
-class Scollcode < Formula
+class ScollCode < Formula
     desc "Local macOS relay for iOS and Codex App Server RPC"
     homepage "https://github.com/ScollCode/homebrew-scollcode"
     version "0.2.0"
